3ebb4bffee739c6c47dca5741c5e09fe2996bbcb
SneakyNinja2041 <135566030+SneakyNinja2041@users.noreply.github.com>
2024-07-07 16:49:16 +1000

    Added station select
    -Players are able to select which station they want to go to
    -Without the player prefs, you will have to change the destination fromthe player object if you want to playtest

---
+++ Assets/Scripts/Menu/MapTest.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapTest : MonoBehaviour
+{
+
+    [SerializeField] GameObject canvas;
+
+
+    private void Start()
+    {
+        canvas.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Map"))
+        {
+            Debug.Log("MAP! ACTIVATE!");
+            canvas.SetActive(true);
+            Cursor.visible = true;
+        }
+    }
+}
