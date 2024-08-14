1c2b2ed99c5677d9391149d4fa996ad71b7b72ac
rangasnafu - Home <135566054+rangasnafu@users.noreply.github.com>
2024-08-14 14:51:33 +1000

    have the stove part of restaurant tutorial working

--- Assets/Scripts/Gameplay/RestaurantTutorial.cs
+++ Assets/Scripts/Gameplay/RestaurantTutorial.cs
@@ -1,3 +1,4 @@
+using DialogueEditor;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,16 +8,39 @@ public class RestaurantTutorial : MonoBehaviour
     bool hasCompletedRTutorial;
     public GameObject tutorialChihuahua;
     public GameObject exclamation;
+    public GameObject foodSelection;
+    public NPCConversation stoveTutorial;
+    public NPCConversation nowWeWait;
+    public NPCConversation beginRestaurantTutorial;
+
+    [SerializeField] private bool stoveTime;
+    [SerializeField] private bool canStoveTutorial = true;
+    //[SerializeField] private bool waitingTime;
+
+    public GameObject panelDialogue;
 
     // Start is called before the first frame update
     void Start()
     {
         hasCompletedRTutorial = ProfileSystem.Get<bool>(ProfileSystem.Variable.RestaurantTutorialDone);
+        ConversationManager.Instance.StartConversation(beginRestaurantTutorial);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (panelDialogue.activeSelf)
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+        
+        if (!panelDialogue.activeSelf && !foodSelection.activeSelf)
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
         hasCompletedRTutorial = ProfileSystem.Get<bool>(ProfileSystem.Variable.RestaurantTutorialDone);
 
         if (!hasCompletedRTutorial)
@@ -28,6 +52,34 @@ public class RestaurantTutorial : MonoBehaviour
         {
             tutorialChihuahua.SetActive(false);
         }
+
+        if (canStoveTutorial)
+        {
+            if (stoveTime && Input.GetKeyDown(KeyCode.E))
+            {
+                BeginStoveTutorial();
+            }
+        }
+        //if (stoveTime && Input.GetKeyDown(KeyCode.E))
+        //{
+        //    BeginStoveTutorial();
+        //}
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            stoveTime = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            stoveTime = false;
+        }
     }
 
     public void RestaurantTutorialCompleted()
@@ -38,5 +90,24 @@ public class RestaurantTutorial : MonoBehaviour
     public void StartTutorial()
     {
         exclamation.SetActive(true);
+        canStoveTutorial = true;
+    }
+
+    private void BeginStoveTutorial()
+    {
+        ConversationManager.Instance.StartConversation(stoveTutorial);
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+
+        canStoveTutorial = false;
+        stoveTime = false;
+    }
+
+    public void WaitConversation()
+    {
+        exclamation.SetActive(false);
+        ConversationManager.Instance.StartConversation(nowWeWait);
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
     }
 }
