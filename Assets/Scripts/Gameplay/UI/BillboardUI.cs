8f990069958c2a274d07cba8c7a0995e72e3d7d2
Alex Hanson <43489279+dippyshere@users.noreply.github.com>
2024-08-21 15:21:36 +1000

    make stove ! prompt always face up, remove f to throw prompt, fix passengers overlapping eve in restaurant tutorial

--- Assets/Scripts/Gameplay/UI/BillboardUI.cs
+++ Assets/Scripts/Gameplay/UI/BillboardUI.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class BillboardUI : MonoBehaviour
 {
     [SerializeField] private Camera _camera;
+    [SerializeField] private bool faceUp = false;
 
     private void Start()
     {
@@ -15,6 +16,15 @@ public class BillboardUI : MonoBehaviour
 
     private void Update()
     {
-        transform.LookAt(transform.position + _camera.transform.rotation * Vector3.forward, _camera.transform.rotation * Vector3.up);
+        if (faceUp)
+        {
+            // rotate to camera, but keep x rotation at 0
+            transform.LookAt(transform.position + _camera.transform.rotation * Vector3.forward, _camera.transform.rotation * Vector3.up);
+            transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
+        }
+        else
+        {
+            transform.LookAt(transform.position + _camera.transform.rotation * Vector3.forward, _camera.transform.rotation * Vector3.up);
+        }
     }
 }
